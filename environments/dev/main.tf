6b7b9ae27ae7676b86256ba2412f35013361d811
CyberBass051 <pb05129000@tutamail.com>
2026-07-27 22:10:07 +0200

    Revert "incident-sim: point WEBHOOK_SECRET_ARN at nonexistent secret (deliberate)"
    This reverts commit c6c5f5303074c14b1164992269159534e8dcc7b2.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -30,7 +30,7 @@ module "webhook_receiver" {
 
   project_name       = "cicd-pipeline"
   pipeline_name      = module.pipeline.pipeline_name
-  webhook_secret_arn = "arn:aws:secretsmanager:us-east-1:221717898536:secret:cicd-pipeline/github-webhook-secret-BROKEN"
+  webhook_secret_arn = aws_secretsmanager_secret.github_webhook.arn
 }
 
 output "webhook_url" {
